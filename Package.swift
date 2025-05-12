9ff268a59ca1b2117bed7aafdc81ebaef9bbbf70
ChenLin Wang <wangchenlin2001@icloud.com>
2025-05-12 12:03:45 +0800

    V0.0.2 将 WhooshingServer 从 whooshing.toolbox-basic 中剥离出来

---
+++ Package.swift
@@ -0,0 +1,47 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "whooshing.toolbox-server",
+    platforms: [
+       .macOS(.v13)
+    ],
+    products: [
+        .library( name: "WhooshingServer", targets: ["WhooshingServer"] )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SJJC-Team/whooshing-vapor.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/SJJC-Team/whooshing-fluent.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/SJJC-Team/whooshing.toolbox-basic.git", .upToNextMajor(from: "1.2.3")),
+        .package(url: "https://github.com/SJJC-Team/whooshing.toolbox-client", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.10.0"),
+        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.82.1"),
+    ],
+    targets: [
+        .target(
+            name: "WhooshingServer",
+            dependencies: [
+                .product(name: "ErrorHandle", package: "whooshing.toolbox-basic"),
+                .product(name: "DataConvertable", package: "whooshing.toolbox-basic"),
+                .product(name: "Cryptos", package: "whooshing.toolbox-basic"),
+                .product(name: "PgSQL", package: "whooshing.toolbox-basic"),
+                .product(name: "WhooshingClient", package: "whooshing.toolbox-client"),
+                .product(name: "Vapor", package: "whooshing-vapor"),
+                .product(name: "Fluent", package: "whooshing-fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
+            ],
+            resources: [
+                .process("Services/API/3.API请求流程.png")
+            ]
+        ),
+        .testTarget(
+            name: "toolbox-server-Tests",
+            dependencies: [
+                .target(name: "WhooshingServer")
+            ]
+        ),
+    ]
+)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# whooshing.toolbox-server
-Whooshing 项目的服务模块依赖库
+# Whooshing 项目的服务模块依赖库
+
+在你的 Package.swift 加入：
+
+``` swift
+.package(url: "https://github.com/SJJC-Team/whooshing.toolbox-server.git", .upToNextMajor(from: "1.0.0"))
+```
