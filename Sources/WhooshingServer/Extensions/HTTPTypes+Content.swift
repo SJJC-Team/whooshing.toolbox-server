3d9822a16ad70e00b3b36161df270d3d63431d22
ChenLin Wang <wangchenlin2001@icloud.com>
2025-05-13 18:54:20 +0800

    V1.0.2.2 更新

--- Sources/WhooshingServer/Extensions/HTTPTypes+Content.swift
+++ Sources/WhooshingServer/Extensions/HTTPTypes+Content.swift
@@ -1,14 +1,39 @@
 import WhooshingClient
 import Vapor
 
-extension HTTPRequest: @retroactive AsyncResponseEncodable {}
-extension HTTPRequest: @retroactive AsyncRequestDecodable {}
-extension HTTPRequest: @retroactive ResponseEncodable {}
-extension HTTPRequest: @retroactive RequestDecodable {}
-extension HTTPRequest: @retroactive Content {}
+extension HTTPResponse: @retroactive AsyncResponseEncodable {
+    public func encodeResponse(for request: Request) async throws -> Response {
+        let body: Response.Body
+        if let buffer = self.body {
+            body = .init(buffer: buffer)
+        } else {
+            body = .empty
+        }
+        let response = Response(
+            status: self.status,
+            version: self.version,
+            headers: self.headers,
+            body: body
+        )
+        return response
+    }
+}
 
-extension HTTPResponse: @retroactive AsyncResponseEncodable {}
-extension HTTPResponse: @retroactive AsyncRequestDecodable {}
-extension HTTPResponse: @retroactive ResponseEncodable {}
-extension HTTPResponse: @retroactive RequestDecodable {}
+extension HTTPResponse: @retroactive ResponseEncodable {
+    public func encodeResponse(for request: Request) -> EventLoopFuture<Response> {
+        let body: Response.Body
+        if let buffer = self.body {
+            body = .init(buffer: buffer, byteBufferAllocator: request.byteBufferAllocator)
+        } else {
+            body = .empty
+        }
+        let response = Response(
+            status: self.status,
+            version: self.version,
+            headers: self.headers,
+            body: body
+        )
+        return request.eventLoop.makeSucceededFuture(response)
+    }
+}
 extension HTTPResponse: @retroactive Content {}
