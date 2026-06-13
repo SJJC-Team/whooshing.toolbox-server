89a7991ec72fb991ebc236239a01afc131aeaf67
ChenLin Wang <wangchenlin2001@icloud.com>
2026-06-14 01:13:17 +0800

    V1.2.3.1 更新 IOS 系统最低要求

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "whooshing.toolbox-server",
     platforms: [
         .macOS(.v13),
-        .iOS(.v14),
+        .iOS(.v16),
         .watchOS(.v6),
         .tvOS(.v13),
     ],
